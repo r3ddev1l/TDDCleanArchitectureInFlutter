2a88974abbc2eb07e34c95e90ee404bbe5ea0131
Srijan <srijanmaharjan1@gmail.com>
2021-03-26 00:37:53 +0545

    test for fromJson and toJson passed

--- lib/features/number_trivia/data/models/number_trivia_model.dart
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -4,4 +4,18 @@ import 'package:meta/meta.dart';
 class NumberTriviaModel extends NumberTrivia {
   NumberTriviaModel({@required String text, @required int number})
       : super(text: text, number: number);
+
+  factory NumberTriviaModel.fromJson(Map<String, dynamic> json) {
+    return NumberTriviaModel(
+      text: json['text'],
+      number: json['number'].toInt(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'text': text,
+      'number': number,
+    };
+  }
 }
